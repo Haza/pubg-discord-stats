f85fdea87b98f964c0afce03350faad234429538
Nicolas Meyer <nicolas@happyculture.coop>
2018-05-02 10:06:08 +0200

    Check if we have matches before looping.

--- prefly.py
+++ prefly.py
@@ -9,10 +9,11 @@ def InitMatchesPerUser(api: PUBG, userList: dict):
     c.execute("CREATE TABLE IF NOT EXISTS matches (match_id varchar(125), username varchar(64))")
     players = api.players().filter(player_names=userList['users'])
     for player in players:
-        for match in player.matches:
-            c.execute("insert into matches values (?, ?)",
-                      [match.id, player.name])
-            conn.commit()
-            print('Adding data for user ' + player.name + ', match ' + match.id)
+        if hasattr(player, matches):
+            for match in player.matches:
+                c.execute("insert into matches values (?, ?)",
+                          [match.id, player.name])
+                conn.commit()
+                print('Adding data for user ' + player.name + ', match ' + match.id)
     # Close db connection
     conn.close()
